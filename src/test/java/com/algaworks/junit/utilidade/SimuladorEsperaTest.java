97f602bb4f1164572f5f335effd443384eb81ce4
Luan Pinheiro da Silva <lluanps@gmail.com>
2023-11-24 01:14:48 -0300

    Desabilitando testes unitários

--- src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
@@ -4,11 +4,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.Duration;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class SimuladorEsperaTest {
 	
 	@Test
+	@Disabled("Não é mais aplicável")
 	public void deveEsperarENaoDarTimeout() {
 		//assertTimeout(Duration.ofSeconds(1), () -> SimuladorEspera.esperar(Duration.ofSeconds(3))); É NECESSARIO ESPERAR OS 3 SEGUNDOS PARA O METODO VALIDAR
 		// assertTimeoutPreemptively(Duration.ofSeconds(1), () -> SimuladorEspera.esperar(Duration.ofSeconds(3))); // desse modo com assertTimeoutPreemptively não a essa necessidade de espera

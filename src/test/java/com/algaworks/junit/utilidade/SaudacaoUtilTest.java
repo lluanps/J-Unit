adb946539941dfbb797aa7cf3967bbeb7f7990b0
Luan Pinheiro da Silva <lluanps@gmail.com>
2023-11-24 17:42:20 -0300

    Adicionado dependencia para realizar test via comand mvn test efeito cobertura de teste para classe SaudacaoUtil

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -14,6 +14,21 @@ public class SaudacaoUtilTest {
 	}
 	
 	@Test
+	public void deveRetornarBomTarde() {
+		String saudacao = SaudacaoUtil.saudar(15);
+		System.out.println(saudacao);
+		assertEquals("Boa tarde", saudacao, "Falha no teste! Saudação incorrenta!");
+	}
+	
+	@Test
+	public void deveRetornarBoaNoite() {
+		String saudacao = SaudacaoUtil.saudar(23);
+		System.out.println(saudacao);
+		assertEquals("Boa noite", saudacao, "Falha no teste! Saudação incorrenta!");
+	}
+		
+	
+	@Test
 	public void deveLancarException() {
 		IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> SaudacaoUtil.saudar(-10));
 		assertEquals("Hora inválida", illegalArgumentException.getMessage());

4c862c689711d435e30d074b8633eb514a3168bc
Luan Pinheiro da Silva <lluanps@gmail.com>
2023-12-06 09:03:39 -0300

    Formatando nomes de testes com @DisplayNameGeneration

--- src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
+++ src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
@@ -5,12 +5,15 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class FiltroNumerosTest {
 	
 	@Test
-	public void deveRetornarNumerosPares() {
+	public void deve_Retornar_Numeros_Pares_() {
 		List<Integer> numeros = Arrays.asList(1, 2, 3, 4);
 		List<Integer> numerosParesEsperados = Arrays.asList(2, 4);
 		List<Integer> resultado = FiltroNumeros.numerosPares(numeros);

318e86632d8a281d5b7c85ddc10ab0d83427084e
Luan Pinheiro da Silva <lluanps@gmail.com>
2023-12-06 09:00:39 -0300

    Alterando nome de exibição dos testes com @DisplayName

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -2,11 +2,14 @@ package com.algaworks.junit.utilidade;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Testes no utilitário de saudação")
 public class SaudacaoUtilTest {
 	
 	@Test
+	@DisplayName("Deve saudar com bom dia")
 	public void deveRetornarBomDiaApartir5Horas() {
 //		Int saudacao = 5;
 		String saudacao = SaudacaoUtil.saudar(5);
@@ -15,6 +18,7 @@ public class SaudacaoUtilTest {
 	}
 	
 	@Test
+	@DisplayName("Deve saudar com boa tarde")
 	public void deveRetornarBomTarde() {
 		String saudacao = SaudacaoUtil.saudar(15);
 		System.out.println(saudacao);
@@ -22,6 +26,7 @@ public class SaudacaoUtilTest {
 	}
 	
 	@Test
+	@DisplayName("Deve saudar com boa noite")
 	public void deveRetornarBoaNoite() {
 		String saudacao = SaudacaoUtil.saudar(2);
 		System.out.println(saudacao);

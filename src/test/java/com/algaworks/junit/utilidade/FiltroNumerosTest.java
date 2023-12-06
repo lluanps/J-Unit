3de52591d38726d8599eab5fb7f78917624ac1dd
Luan Pinheiro da Silva <lluanps@gmail.com>
2023-12-06 09:11:27 -0300

    Aplicando a nomenclatura do BDD para nomear métodos de teste

--- src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
+++ src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
@@ -12,13 +12,25 @@ import org.junit.jupiter.api.Test;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class FiltroNumerosTest {
 	
+	//GIVEN, WHEN , THEN
+	//DADO, QUANDO, ENTAO
 	@Test
-	public void deve_Retornar_Numeros_Pares_() {
+	public void Dado_uma_lista_de_numeros_Quando_fitrar_por_pares_Entao_deve_retornar_apenas_numeros_pares() {
 		List<Integer> numeros = Arrays.asList(1, 2, 3, 4);
 		List<Integer> numerosParesEsperados = Arrays.asList(2, 4);
 		List<Integer> resultado = FiltroNumeros.numerosPares(numeros);
 
 		assertIterableEquals(numerosParesEsperados, resultado);
+		// outra forma de realziar  -> assertArrayEquals(numerosParesEsperados.toArray(new Object[] {}), resultado.toArray(new Object[] {}));
+	}
+	
+	@Test
+	public void Dado_uma_lista_de_numeros_Quando_fitrar_por_impares_Entao_deve_retornar_apenas_numeros_pares() {
+		List<Integer> numeros = Arrays.asList(1, 2, 3, 4);
+		List<Integer> numerosImparesEsperados = Arrays.asList(1, 3);
+		List<Integer> resultado = FiltroNumeros.numerosImpares(numeros);
+
+		assertIterableEquals(numerosImparesEsperados, resultado);
 		// outra forma de realziar  -> assertArrayEquals(numerosParesEsperados.toArray(new Object[] {}), resultado.toArray(new Object[] {}));
 	}
 
